9ea79eb02aa7664c625b73123850e1c9e5da3697
krrish6887 <krrish01sahu@gmail.com>
2024-11-30 07:43:17 +0530

    Update Arithmetic operations.py

--- Arithmetic operations.py
+++ Arithmetic operations.py
@@ -1,1 +1,9 @@
 # Python
+#arithematic operation
+a=int(input("Enter 1st number="))
+b=int(input("Enter 2nd number="))
+print("Sum=",a+b)
+print("Difference=",a-b)
+print("Product=",a*b)
+print("Division=",a/b)
+print("Modulo=",a%b)
