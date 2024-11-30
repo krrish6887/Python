008b4a6223c72cda1ee769bd9805c374191decb3
krrish6887 <krrish01sahu@gmail.com>
2024-11-30 08:10:33 +0530

    Create Logical_operator.py

---
+++ Logical_operator.py
@@ -0,0 +1,13 @@
+#Logical operator
+a=int(input("Enter 1st number="))
+b=int(input("Enter 2nd number="))
+
+print("Logical operation : boolean values")
+print("a and b :",a and b)
+print("not a and b :",not a and b)
+print("a and not b :",a and not b)
+print("not a and not b :",not a and not b)
+print("a or b :",a or b)
+print("not a or b :",not a or b)
+print("a or not b :",a or not b)
+print("not a or not b :",not a or not b)
