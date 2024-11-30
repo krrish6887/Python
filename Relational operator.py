5135fbd47b39c3968f1f33886cbe5c72f0327d8c
krrish6887 <krrish01sahu@gmail.com>
2024-11-30 07:52:16 +0530

    Create Relational operator.py

---
+++ Relational operator.py
@@ -0,0 +1,11 @@
+#Relational operator
+a=int(input("Enter 1st number="))
+b=int(input("Enter 2nd number="))
+
+print("condition : boolean values")
+print("a>b :",a>b)
+print("a>=b :",a>=b)
+print("a<b :",a<b)
+print("a<=b :",a<=b)
+print("a==b :",a==b)
+print("a!=b :",a!=b)
