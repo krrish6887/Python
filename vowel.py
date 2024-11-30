21785bf8afc8a1cb9d374a28d239a9eb0bd8941b
krrish6887 <krrish01sahu@gmail.com>
2024-11-30 18:36:18 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+#Program to know if a character is vowel or not
+c=input("Enter a character:")
+
+if(c=='a'or c=='e' or c=='i'or c=='o' or c=='u'or c=='A' or c=='E'or c=='I'or c=='O'or c=='U'):
+    print("VOWEL")
+else:
+    print("Not a VOWEL")
